99e7e62f322fdad888bfc352ff9cdb2fe3a64a3a
Arjun Patil <arjunshivaji100@gmail.com>
2021-04-30 19:08:47 +0530

    Updated index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hellow world</h1>
+    <p>The first paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
